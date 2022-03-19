f4c99b151b94286e24fe3a608b8c6eb3cb5d1a8b
Sairam Polina <48856345+sairampolina@users.noreply.github.com>
2022-03-19 14:30:43 -0400

    updated taking inputs

--- project3_1.py
+++ project3_1.py
@@ -30,59 +30,99 @@ def createObstacles(canvas):
                 canvas[j][i]=[0,255,0]
  
     return canvas
+
 start_state=[]
+goal_state=[]
 
 def getInputs():
+    
+    # get valid x,y,theta_s coordinate of start point
     while True:
         
         while True:
             
             value=input("Enter the X- coordinate of Start Point: ")
             
-            if value<0 or value>a_width:
-                print("Invalid input...Re-enter a X-coordinate with\n")
+            if float(value)<0 or float(value)>a_width:
+                print("Invalid input...Re-enter a valid X-coordinate \n")
                 continue
             else:
-                start_state.append(value)
+                start_state.append(float(value))
                 break
         while True:
             
             value=input("Enter Y-coordinate of Start Point: ")
             
-            if value<0 or value>a_height:
+            if float(value)<0 or float(value)>a_height:
                 print("Invalid Input...Re-enter a valid Y-coordinate\n")
                 continue
             else:
-                start_state.append(value)
+                start_state.append(float(value))
                 break
         
         while True:
             
-            value=input("Enter Orientation of Robot at Start Point(+ve in counter-clockwise): ")
+            value=input("Enter Orientation of Robot at Start Point(+ve in counter-clockwise):\n")
             
-            start_state.append(value)
+            start_state.append(float(value))
             break
         
-        if canvas[a_height-start_state[1]][start_state[0]][2]==255:
-            print("Start Point Entered is inside Obstacle Space: ")
+        
+        if (canvas[a_height-int(start_state[1])][int(start_state[0])][1]==255):
+            print("***Start Point Entered is inside Obstacle Space..Retry***")
+            start_state.clear()
             continue
         else:
             break
     
-   
+    # get valid x,y,theta_g coordinate of goal node
+    while True:
+        
+        while True:
+            
+            value=input("Enter the X- coordinate of Goal Point: ")
+            
+            if float(value)<0 or float(value)>a_width:
+                print("Invalid input...Re-enter a valid X-coordinate \n")
+                
+                continue
+            else:
+                goal_state.append(float(value))
+                break
+        
+        while True:
+            
+            value=input("Enter Y-coordinate of Goal Point: ")
+            
+            if float(value)<0 or float(value)>a_height:
+                print("Invalid Input...Re-enter a valid Y-coordinate\n")
+                continue
+            else:
+                goal_state.append(float(value))
+                break
+        
+        while True:
+            
+            value=input("Enter Orientation of Robot at Goal Point(+ve in counter-clockwise): \n")
+            
+            goal_state.append(float(value))
+            break
+        
+        if canvas[a_height-int(goal_state[1])][int(goal_state[0])][1]==255:
+            print("***Goal Point Entered is inside Obstacle Space..Retry***")
+            goal_state.clear()
+            continue
+        else:
+            clearence=input('Enter Clearence of robot to obstacles:')
+            robot_radius=input('Enter radius of Robot: ')
+            step_size=input('Enter the step-size for robot movement:')
+            break
+
     
+    return start_state,goal_state,clearence,robot_radius,step_size
        
     
     
-        
-
-
-
-
-
-
-
-
 
 canvas=np.ones((250,400,3),dtype='uint8')
 canvas=createObstacles(canvas)
@@ -90,6 +130,16 @@ canvas=createObstacles(canvas)
 height,width,_=canvas.shape
 a_height=height-1
 a_width=width-1
-cv2.imshow('CANVAS',canvas)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+
+start_state,goal_state,clearence,robot_radius,step_size=getInputs()
+
+
+# changing world co-ordinates to map-coordinates
+start_state[1]=a_height-start_state[1]
+goal_state[1]=a_height-goal_state[1]
+
+
+# print(goal_state)
+# cv2.imshow('CANVAS',canvas)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
